c3df7acdaf5ee9512e6f23043db390bd7cc8d9b8
Tazhibaev Iskhak <72846050+tottsss@users.noreply.github.com>
2026-02-14 20:45:37 +0400

    Update liquidity_Final.py

--- ready_bots/liquidity_Final.py
+++ ready_bots/liquidity_Final.py
@@ -14,15 +14,15 @@ HEADERS = {"X-API-key": API_KEY}
 SHUTDOWN = False
 
 # Strategy params
-MIN_EDGE = float(os.environ.get("RIT_FINAL_MIN_EDGE", "0.15"))
+MIN_EDGE = float(os.environ.get("RIT_FINAL_MIN_EDGE", "0.08")) # Minimum edge for the tender to be accepted, default is 0.15
 # Minimum edge for the tender to be accepted
 
 
-VOL_FACTOR = float(os.environ.get("RIT_FINAL_VOL_FACTOR", "1.1")) # Volume factor for the tender to be accepted, default is 1.2
+VOL_FACTOR = float(os.environ.get("RIT_FINAL_VOL_FACTOR", "1.2")) # Volume factor for the tender to be accepted, default is 1.2
 MAX_ATTEMPTS = int(os.environ.get("RIT_FINAL_MAX_ATTEMPTS", "8"))# Maximum number of attempts to evaluate the tender    
 EVAL_DELAY = float(os.environ.get("RIT_FINAL_EVAL_DELAY", "1.0"))# Delay between attempts to evaluate the tender
-ORDER_DELAY = float(os.environ.get("RIT_FINAL_ORDER_DELAY", "0.08"))# Delay between orders, default is 0.15
-AFTER_ACCEPT_DELAY = float(os.environ.get("RIT_FINAL_AFTER_ACCEPT_DELAY", "0.6"))# Delay after accepting the tender
+ORDER_DELAY = float(os.environ.get("RIT_FINAL_ORDER_DELAY", "0.15"))# Delay between orders, default is 0.15
+AFTER_ACCEPT_DELAY = float(os.environ.get("RIT_FINAL_AFTER_ACCEPT_DELAY", "0.5"))# Delay after accepting the tender, default is 0.6
 MAX_ORDER_SIZE = 10000.0# Maximum order size
 ENDGAME_TICKS = int(os.environ.get("RIT_FINAL_ENDGAME_TICKS", "8"))# Number of ticks to end the game
 FIXED_ONLY = os.environ.get("RIT_FINAL_FIXED_ONLY", "1").strip() in {"1", "true", "yes", "on"}# Whether to only accept fixed tenders
